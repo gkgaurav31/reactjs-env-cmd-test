45c4cc91357bbabaf799291bbe6b761870fbc2ed
gkgaurav31 <gkgaurav31@gmail.com>
2021-05-18 13:03:38 +0530

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>{process.env.REACT_APP_TEST}</h1>;
         </a>
       </header>
     </div>
